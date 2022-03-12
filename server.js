a277d2aad3c39772de029a14ddfc117f9930ac90
Revina-M <revina95rm@gmail.com>
2022-03-13 00:33:33 +0530

    cc

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const db = require("./db.js");
 const dotenv = require("dotenv");
 const jobsRoute = require("./routes/jobsRoute");
 const usersRoute = require("./routes/usersRoute");
+// const path = require("path");
 
 dotenv.config();
 
 app.use(express.json());
 app.use("/api/jobs", jobsRoute);
 app.use("/api/users", usersRoute);
+
+// -------deployment------
+
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
+
+// -------deployment------
 
 const {
   notFound,
