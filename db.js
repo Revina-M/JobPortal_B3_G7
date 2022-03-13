624789e44a6347641373bfc4c0a34cf7236307b9
Revina-M <revina95rm@gmail.com>
2022-03-13 07:37:50 +0530

    cc
    cc

--- db.js
+++ db.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
-//database connection
+require("dotenv").config();
 dbConnect();
 async function dbConnect() {
   try {
-    // await mongoose.connect(
-    //   "mongodb+srv://FSDB3group7:FSDB3group7@ictakcluster.rpo6w.mongodb.net/JOBPORTAL?retryWrites=true&w=majority",
-    //   { useNewUrlParser: true }
-    // );
-    await mongoose.connect(
-      "mongodb+srv://userone:userone@ictakfiles.mxjse.mongodb.net/JOBPORTAL?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-      }
-    );
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+    console.log("MongoDB connection successful");
   } catch (error) {
     console.log("Mongo DB connection unsuccessful");
   }
